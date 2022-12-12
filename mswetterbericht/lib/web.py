babe5f22132bdf26f4c19b733690a2ecbb0fd72e
Till Wiese <1872304+m3adow@users.noreply.github.com>
2022-12-12 12:23:32 +0100

    Add possibility to add "additional_headers" to resilient_request

--- mswetterbericht/lib/web.py
+++ mswetterbericht/lib/web.py
@@ -1,19 +1,25 @@
-import cloudscraper
 import logging
-from requests.models import Response
 from time import sleep
+
+import cloudscraper
+from requests.models import Response
 
 logger = logging.getLogger(__name__)
 
 
-def resilient_request(url: str, retries: int = 3, backoff_factor: int = 5) -> Response:
+def resilient_request(
+    url: str, retries: int = 3, backoff_factor: int = 5, additional_headers=None
+) -> Response:
     """Do requests with retries on 5XX errors"""
+    if additional_headers is None:
+        additional_headers = {}
     current_try = 0
     backoff_time = backoff_factor
+    scraper = cloudscraper.CloudScraper(interpreter="py2js")
+    scraper.headers.update(additional_headers)
     # Manually implement retries as I couldn't get the "normal" requests version working with cloudscraper
     while current_try <= retries:
-        scraper = cloudscraper.create_scraper(interpreter="py2js")
-        r = scraper.get(url)
+        r: Response = scraper.get(url)
         if r.status_code == 200:
             break
         elif r.status_code <= 500 < 600:
@@ -33,4 +39,5 @@ def resilient_request(url: str, retries: int = 3, backoff_factor: int = 5) -> Re
         )
         exit(1)
 
+    # noinspection PyUnboundLocalVariable
     return r
